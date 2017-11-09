d5a6db651972aa8bce0672e237ea24d32fdc0482
BrianLM <brian.distefano@libertymutual.com>
2017-11-09 00:26:59 -0500

    Removed console.log

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -353,7 +353,6 @@ const addProduct = function (event) {
   event.preventDefault()
 
   const data = getFormFields(event.target)
-  console.log(data)
   itemApi.addItem(data)
     .then(() => {
       ui.addProductToggle()
